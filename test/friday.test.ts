cc22410b640927c9151fb47cd51144fbc5921df5
Roman Ožana <roman@ozana.cz>
2025-01-06 10:37:32 +0100

    Fix format

--- test/friday.test.ts
+++ test/friday.test.ts
@@ -3,30 +3,30 @@ import { getGoodFriday, isGoodFriday } from "../lib";
 import { fromIsoDate, toIsoDate } from "../lib/date-fns.js";
 
 describe("Good Friday date tests", () => {
-  it("Good Friday date for 10 years", () => {
-    expect(toIsoDate(getGoodFriday(2024))).toBe("2024-03-29");
-    expect(toIsoDate(getGoodFriday(2025))).toBe("2025-04-18");
-    expect(toIsoDate(getGoodFriday(2026))).toBe("2026-04-03");
-    expect(toIsoDate(getGoodFriday(2027))).toBe("2027-03-26");
-    expect(toIsoDate(getGoodFriday(2028))).toBe("2028-04-14");
-    expect(toIsoDate(getGoodFriday(2029))).toBe("2029-03-30");
-    expect(toIsoDate(getGoodFriday(2030))).toBe("2030-04-19");
-    expect(toIsoDate(getGoodFriday(2031))).toBe("2031-04-11");
-    expect(toIsoDate(getGoodFriday(2032))).toBe("2032-03-26");
-    expect(toIsoDate(getGoodFriday(2033))).toBe("2033-04-15");
-  });
+	it("Good Friday date for 10 years", () => {
+		expect(toIsoDate(getGoodFriday(2024))).toBe("2024-03-29");
+		expect(toIsoDate(getGoodFriday(2025))).toBe("2025-04-18");
+		expect(toIsoDate(getGoodFriday(2026))).toBe("2026-04-03");
+		expect(toIsoDate(getGoodFriday(2027))).toBe("2027-03-26");
+		expect(toIsoDate(getGoodFriday(2028))).toBe("2028-04-14");
+		expect(toIsoDate(getGoodFriday(2029))).toBe("2029-03-30");
+		expect(toIsoDate(getGoodFriday(2030))).toBe("2030-04-19");
+		expect(toIsoDate(getGoodFriday(2031))).toBe("2031-04-11");
+		expect(toIsoDate(getGoodFriday(2032))).toBe("2032-03-26");
+		expect(toIsoDate(getGoodFriday(2033))).toBe("2033-04-15");
+	});
 
-  it("Good Friday date for 100 years", () => {
-    // 2105 and above
-    expect(toIsoDate(getGoodFriday(2105))).toBe("2105-04-03");
-    expect(toIsoDate(getGoodFriday(2106))).toBe("2106-04-16");
+	it("Good Friday date for 100 years", () => {
+		// 2105 and above
+		expect(toIsoDate(getGoodFriday(2105))).toBe("2105-04-03");
+		expect(toIsoDate(getGoodFriday(2106))).toBe("2106-04-16");
 
-    // 2200 and above
-    expect(toIsoDate(getGoodFriday(2225))).toBe("2225-03-25");
-    expect(toIsoDate(getGoodFriday(2299))).toBe("2299-04-14");
-  });
+		// 2200 and above
+		expect(toIsoDate(getGoodFriday(2225))).toBe("2225-03-25");
+		expect(toIsoDate(getGoodFriday(2299))).toBe("2299-04-14");
+	});
 
-  it("Good Friday in 2024", () => {
-    expect(isGoodFriday(fromIsoDate("2024-03-29"))).toBe(true);
-  });
+	it("Good Friday in 2024", () => {
+		expect(isGoodFriday(fromIsoDate("2024-03-29"))).toBe(true);
+	});
 });
--- test/holy-week.test.ts
+++ test/holy-week.test.ts
@@ -3,17 +3,17 @@ import { getHolyWeek } from "../lib";
 import { toIsoDate } from "../lib/date-fns.js";
 
 describe("Holy Week in 2024", () => {
-  it("should return correct dates", () => {
-    const holyWeek = getHolyWeek(2024);
+	it("should return correct dates", () => {
+		const holyWeek = getHolyWeek(2024);
 
-    expect(toIsoDate(holyWeek.palmSunday.date)).toBe("2024-03-24");
-    expect(toIsoDate(holyWeek.holyMonday.date)).toBe("2024-03-25");
-    expect(toIsoDate(holyWeek.holyTuesday.date)).toBe("2024-03-26");
-    expect(toIsoDate(holyWeek.holyWednesday.date)).toBe("2024-03-27");
-    expect(toIsoDate(holyWeek.maundyThursday.date)).toBe("2024-03-28");
-    expect(toIsoDate(holyWeek.goodFriday.date)).toBe("2024-03-29");
-    expect(toIsoDate(holyWeek.holySaturday.date)).toBe("2024-03-30");
-    expect(toIsoDate(holyWeek.easterSunday.date)).toBe("2024-03-31");
-    expect(toIsoDate(holyWeek.easterMonday.date)).toBe("2024-04-01");
-  });
+		expect(toIsoDate(holyWeek.palmSunday.date)).toBe("2024-03-24");
+		expect(toIsoDate(holyWeek.holyMonday.date)).toBe("2024-03-25");
+		expect(toIsoDate(holyWeek.holyTuesday.date)).toBe("2024-03-26");
+		expect(toIsoDate(holyWeek.holyWednesday.date)).toBe("2024-03-27");
+		expect(toIsoDate(holyWeek.maundyThursday.date)).toBe("2024-03-28");
+		expect(toIsoDate(holyWeek.goodFriday.date)).toBe("2024-03-29");
+		expect(toIsoDate(holyWeek.holySaturday.date)).toBe("2024-03-30");
+		expect(toIsoDate(holyWeek.easterSunday.date)).toBe("2024-03-31");
+		expect(toIsoDate(holyWeek.easterMonday.date)).toBe("2024-04-01");
+	});
 });
