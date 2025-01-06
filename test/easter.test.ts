bfaa310ffbf43f228e99553079b7210ca76fa53c
Roman Ožana <roman@ozana.cz>
2025-01-06 10:38:54 +0100

    Fix format

--- test/easter.test.ts
+++ test/easter.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import { getEaster, isEaster } from "../lib";
 import { fromIsoDate } from "../lib/date-fns.js";
 
